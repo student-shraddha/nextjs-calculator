008449cb699a3f72e0a9c622c7357627fedaa69c
student-shraddha <shraddhasoni2566@gmail.com>
2024-10-26 19:54:26 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-# Install Node.js if it’s not already installed
-curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-sudo apt-get install -y nodejs
+
+# Update the package list and install dependencies for Amazon Linux
+sudo yum update -y
+sudo yum install -y ruby wget
+
+# Install Node.js and npm
+curl -sL https://rpm.nodesource.com/setup_18.x | sudo bash -
+sudo yum install -y nodejs
 
 # Install PM2 to manage the Next.js app process
 sudo npm install -g pm2
 
 # Navigate to app directory and install dependencies
-cd /home/ubuntu/nextjs-calculator
+cd /home/ec2-user/nextjs-calculator || exit 1
 npm install
