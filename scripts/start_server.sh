3990f0164063ac0e3ee0227358b114599394989d
student-shraddha <shraddhasoni2566@gmail.com>
2024-10-26 19:44:10 +0530

    Create start_server.sh

---
+++ scripts/start_server.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Navigate to the app directory
+cd /home/ubuntu/nextjs-calculator
+
+# Build the Next.js app
+npm run build
+
+# Start the Next.js app using PM2
+pm2 start npm --name "nextjs-calculator" -- start
+pm2 save
